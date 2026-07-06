b2dc9cfb2f5b8d4e142984a34072cd1993e11075
Gabriel Tang <131333781+gabrieltang515@users.noreply.github.com>
2026-07-06 20:57:15 +0800

    Time: 30 ms (87.47%) | Memory: 12.8 MB (31.83%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        hashmap = {} #stores original node -> new node mapping
+        if node is None:
+            return None
+
+            
+        def dfs(node):
+            if node in hashmap:
+                return hashmap[node]
+
+            clone = Node(node.val, [])
+            hashmap[node] = clone
+
+            for neighbor in node.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+
+            return clone
+
+
+        return dfs(node)
+
+
