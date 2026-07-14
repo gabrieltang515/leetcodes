6362f2989e2fc8c8303f0dec0482381d56be1b8b
Gabriel Tang <131333781+gabrieltang515@users.noreply.github.com>
2026-07-14 09:13:45 +0800

    Time: 4 ms (51.73%) | Memory: 14.1 MB (29.05%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -4,15 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        hashmap = {}
+        # hashmap = {}
 
-        for ele in nums:
-            if ele in hashmap:
-                hashmap[ele] = hashmap[ele] + 1
-            else:
-                hashmap[ele] = 1
+        # for ele in nums:
+        #     if ele in hashmap:
+        #         hashmap[ele] = hashmap[ele] + 1
+        #     else:
+        #         hashmap[ele] = 1
 
-        for ele in nums:
-            if hashmap[ele] == 1:
-                return ele
+        # for ele in nums:
+        #     if hashmap[ele] == 1:
+        #         return ele
+        xor_sum = 0
+        for i in range(len(nums)):
+            xor_sum = xor_sum ^ nums[i]
+
+        return xor_sum
+            
+
             
