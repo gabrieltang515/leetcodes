ab1abc236b13788898b6f9fbfa61609d8df268f6
Gabriel Tang <131333781+gabrieltang515@users.noreply.github.com>
2026-09-13 22:43:31 +0800

    Time: 248 ms (45.69%) | Memory: 32.3 MB (7.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,34 +1,38 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        if not grid:
-            return 0
-
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         rows = len(grid)
-        cols = len(grid[0])
+        columns = len(grid[0])
+        hashmap = {}
         islands = 0
 
-        def dfs(r, c):
-            # Out of bounds or water
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "0":
+        def dp(row, col):
+
+            if (row, col) in hashmap:
+                return 
+
+            if row < 0 or row >= rows or col < 0 or col >= columns:
                 return
 
-            # Mark current land as visited
-            grid[r][c] = "0"
+            if grid[row][col] == "0":
+                return
 
-            # Explore 4 directions
-            dfs(r + 1, c)  # down
-            dfs(r - 1, c)  # up
-            dfs(r, c + 1)  # right
-            dfs(r, c - 1)  # left
+            grid[row][col] = "0"
+            hashmap[(row, col)] = True
+
+            dp(row, col + 1)
+            dp(row, col - 1)
+            dp(row + 1, col)
+            dp(row - 1, col)
+
+
 
         for r in range(rows):
-            for c in range(cols):
+            for c in range(columns):
                 if grid[r][c] == "1":
                     islands += 1
-                    dfs(r, c)
+                    dp(r, c)
+
 
         return islands
+
+            
